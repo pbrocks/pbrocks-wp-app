5f70a7d8104d4da2a817cb3e0f6392c0f72b81ed
Paul Barthmaier <pbrocks@users.noreply.github.com>
2022-04-06 09:19:02 -0400

    Update pbrocks-wp-app.php

--- pbrocks-wp-app.php
+++ pbrocks-wp-app.php
@@ -1,11 +1,11 @@
 <?php
 /**
- * Plugin Name:       Tabbed Content
- * Description:       Example block written with ESNext standard and JSX support – build step required.
+ * Plugin Name:       PBrocks WP App
+ * Description:       Sample code written with ESNext standard and JSX support.
  * Requires at least: 5.8
  * Requires PHP:      7.0
- * Version:           0.1.0
- * Author:            The WordPress Contributors
+ * Version:           0.1.1
+ * Author:            The WordPress Contributors & pbrocks
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain:       pbrocks-wp-app
